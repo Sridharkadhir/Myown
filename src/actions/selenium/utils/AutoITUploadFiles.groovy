52f433e1cee52fb7ba233619a1e286d6443f8a57
SRIV2E11114 <sriV2E11114@VEE2IT.COM>
2017-03-29 18:04:50 +0530

    Made some changes and now its working perfectly .

--- src/actions/selenium/utils/AutoITUploadFiles.groovy
+++ src/actions/selenium/utils/AutoITUploadFiles.groovy
@@ -4,11 +4,14 @@ import actions.selenium.utils.Elements;
 import actions.selenium.Browser;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.Keys;
-
+import java.util.concurrent.TimeUnit;
 
 class AutoITUploadFiles{
-    public void run(def params){
+    public void run(def params)
+    {
         Runtime.getRuntime().exec(params.Path);
+        
+        // Path should be of the .exe file that you want to upload ( through which the targetter file will get uploaded)
     }
 }
 
