0ec283bb28ca966c55cb2877f162d03983cc90fc
SRIV2E11114 <sriV2E11114@VEE2IT.COM>
2017-04-14 18:39:01 +0530

    Script to fetch the selected value from the drop down is added - 14 April 2017

---
+++ src/actions/selenium/utils/DropDownSelectedText.groovy
@@ -0,0 +1,32 @@
+package actions.selenium;
+
+
+import actions.selenium.utils.Elements
+import actions.selenium.Browser
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.ui.Select
+
+class DropDownSelectedText{
+  
+  public run(def params){
+    WebElement element = Elements.find(params,Browser.Driver)
+    
+      
+      String selectedValue = new Select(element).getFirstSelectedOption().getText();
+      
+      assert selectedValue.equals(params.Text), "Error expected text: ${params.Text} does not match text on the UI: ${selectedValue}"
+      
+      
+      
+      
+      
+      
+      
+      return selectedValue;
+    
+    	//new Select(element).selectByVisibleText(params."Visible Text")  
+    
+	
+    
+  }
+}
